8e152bf44c962bd703ea7ff51df08253c62dfcd5
haytakm <mrt.hayta@gmail.com>
2024-06-02 21:58:10 +0200

    Swift training initial commit

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -4,21 +4,58 @@
 //
 //  Created by Hayta on 2.06.2024.
 //
-
+let myColor = #colorLiteral(red: 0.8321695924, green: 0.985483706, blue: 0.4733308554, alpha: 1)
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day:"Sunday",high:21,low:14, isRainy: false)
+            DayForecast(day:"Monday",high:18,low:13, isRainy: true)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let high: Int
+    let low: Int
+    let isRainy: Bool
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "cloud.sun.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(Font.largeTitle)
+                .padding(1)
+            Text("High: \(high)")
+                .fontWeight(.semibold)
+            Text("Low: \(low)")
+                .fontWeight(.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
 }
